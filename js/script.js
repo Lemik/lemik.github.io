b94c552b390b1f62e3c7a9c2b2454c315e18be32
Lemik <lemik@i.ua>
2016-05-24 23:05:36 -0700

    new

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ $(function () {
   $(window).on("load resize", function () {
     $(".fill-screen").css("height", window.innerHeight);
   });
+//video 
+//  $("#video-wallpaper").wallpaper({
+//    source: {
+//        poster: "images/2x1200x600.jpg",
+//        mp4: "video.mp4"
+//       // video: "//www.youtube.com/embed/plW58zpcmpk"
+//    }
+//   });
+
 });
